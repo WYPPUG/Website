38062d35bf727bae78376f7fb5717f1d397a5d6b
Rob Wood <rob@rnwood.co.uk>
2023-03-13 07:35:03 +0000

    Add missing change

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,65 +1,136 @@
-import { CreateSchemaCustomizationArgs, GatsbyNode, graphql } from "gatsby"
-import path from "path"
+import { CreateSchemaCustomizationArgs, GatsbyNode, graphql } from "gatsby";
+import path from "path";
 
-export const createSchemaCustomization : GatsbyNode["createSchemaCustomization"] = ({ actions, schema, getNode }) => {
-  actions.createTypes([
-    schema.buildObjectType({
-      name: 'Mdx',
-      interfaces: ['Node'],
-      fields: {
-        isFuture: {
-          type: 'Boolean!',
-          resolve: (s : any) => new Date(s.frontmatter.endDate) >= new Date(),
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
+  ({ actions, schema, getNode }) => {
+    actions.createTypes([
+      schema.buildObjectType({
+        name: "Mdx",
+        interfaces: ["Node"],
+        fields: {
+          isFuture: {
+            type: "Boolean!",
+            resolve: (s: any) => new Date(s.frontmatter.endDate) >= new Date(),
+          },
         },
-      },
-    }),
-  ])
-}
+      }),
+    ]);
+  };
 
-export const createPages : GatsbyNode["createPages"] = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
-  const result : any = await graphql(`
-  query PagesQuery {
-    allFile(
-      filter: {
-        sourceInstanceName: {eq: "mdxpages"}
-        childMdx:{id:{ne: null}}
-      }
-    ) {
-      edges {
-        node {
-          internal {
-            contentFilePath
+export const createPages: GatsbyNode["createPages"] = async ({
+  graphql,
+  actions,
+  reporter,
+}) => {
+  const { createPage } = actions;
+
+  async function makePagesWithQuery<T>(
+    query: Promise<{ errors?: any; data?: T }>,
+    templatePath: string,
+    pathPrefix: string,
+    contextGetter: (page: T) => any
+  ) {
+    const result: any = await query;
+    if (result.errors) {
+      reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+    }
+
+    const pages = result.data?.allFile.edges;
+
+    pages.forEach((edge: any) => {
+      createPage({
+        path: `${pathPrefix}/${edge.node.name}`,
+        component:
+          path.resolve(templatePath) +
+          `?__contentFilePath=${edge.node.childMdx.internal.contentFilePath}`,
+        context: contextGetter(edge),
+      });
+    });
+  }
+
+  await makePagesWithQuery<any>(
+    graphql(`
+      query PagesQuery {
+        allFile(
+          filter: {
+            sourceInstanceName: { eq: "mdxpages" }
+            childMdx: { id: { ne: null } }
           }
-          id
-          name
-          childMdx {
-            internal {
-              contentFilePath
+        ) {
+          edges {
+            node {
+              internal {
+                contentFilePath
+              }
+              id
+              name
+              childMdx {
+                internal {
+                  contentFilePath
+                }
+                id
+                isFuture
+                frontmatter {
+                  title
+                }
+                body
+              }
             }
-            id
-            isFuture
-            frontmatter {
-              title
-            }
-            body
           }
         }
       }
-    }
-  }
-  `)
-  if (result.errors) {
-      reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
-  }
-  
-  const pages = result.data?.allFile.edges;
+    `),
+    "./src/templates/mdxlayout.tsx",
+    "",
+    (edge) => ({ title: edge.node.childMdx.frontmatter.title })
+  );
 
-  pages.forEach( (edge:any) => {
-      createPage({
-          path: `/${edge.node.name}`,
-          component: path.resolve('./src/components/mdxlayout.tsx') + `?__contentFilePath=${edge.node.childMdx.internal.contentFilePath}`,
-          context: { title: edge.node.childMdx.frontmatter.title },
-      })
-  })
-}
+  await makePagesWithQuery<any>(
+    graphql(`
+      query EventPagesQuery {
+        allFile(
+          filter: {
+            sourceInstanceName: { eq: "mdxevents" }
+            childMdx: { id: { ne: null } }
+          }
+        ) {
+          edges {
+            node {
+              internal {
+                contentFilePath
+              }
+              id
+              name
+              childMdx {
+                internal {
+                  contentFilePath
+                }
+                id
+                isFuture
+                frontmatter {
+                  title
+                  summary
+                  startDate
+                  endDate
+                  displayDate
+                  preMeetingLinks {
+                    url
+                    name
+                  }
+                  postMeetingLinks {
+                    url
+                    name
+                  }
+                }
+                body
+              }
+            }
+          }
+        }
+      }
+    `),
+    "./src/templates/mdxeventlayout.tsx",
+    "/events",
+    (edge) => ({ event: edge.node.childMdx })
+  );
+};
